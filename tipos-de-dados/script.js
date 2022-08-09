3415522254870a272931d0c8d9a73c37da7e6189
William Dias <smdwilliam89@gmail.com>
2022-08-09 20:43:30 +0100

    Tipos de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,58 @@
+var nome = 'William';
+var idade = 33;
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+var nome = 'Bárbara';
+var sobrenome = 'Moreira';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase1 = 'Romário fez ' + gols + ' gols';
+var frase2 = `Romário fez ${gols} gols`; // Template String
+console.log(frase1);
+console.log(frase2);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = 'teste';
+var frase1 = 'Esse é o "melhor" jogo';
+console.log(frase1);
+
+// EXERCÍCIO
+
+console.log('');
+console.log('///// EXERCÍCIO /////');
+console.log('');
+
+// Declare uma variável contendo uma string
+var nome = 'Laura';
+console.log(typeof nome);
+
+// Declare uma variável contendo um número dentro de uma string
+var num = '29';
+console.log(num);
+
+// Declare uma variável com a sua idade
+var idade = 33;
+console.log(idade);
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var nome = 'William';
+var sobrenome = 'Dias';
+var nomeCompleto = `Meu nome completo é ${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
+
+// 
