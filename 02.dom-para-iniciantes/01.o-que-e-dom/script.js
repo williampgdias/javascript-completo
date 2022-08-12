40b3aaaaa6a06acbd89446aed98fcaf6652cd3d1
William Dias <smdwilliam89@gmail.com>
2022-08-12 18:57:38 +0100

    O que é DOM 2

--- 02.dom-para-iniciantes/01.o-que-e-dom/script.js
+++ 02.dom-para-iniciantes/01.o-que-e-dom/script.js
@@ -12,6 +12,26 @@ if (href === 'http://127.0.0.1:5500/o-que-e-dom/index.html') {
 
 const h1Selecionado = document.querySelector('h1');
 
-console.log(h1Selecionado);
+function handleClick() {
+  console.log('Clicou em ', h1Selecionado.innerText);
+}
 
-h1Selecionado.innerHTML = 'Oie'
+h1Selecionado.addEventListener('click', handleClick);
+
+// EXERCÍCIO
+
+// RETORNE O URL DA PÁGINA ATUAL UTILIZANDO O OBJETO WINDOW
+const hrefPagina = window.location.href;
+console.log(hrefPagina);
+
+// SELECIONA O PRIMEIRO ELEMENTO DA PÁGINA QUE POSSUA A CLASSE ATIVO
+const elementoAtivo = document.querySelector('.ativo');
+console.log(elementoAtivo);
+
+// RETORNE A LINGUAGEM DO NAVEGADOR
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
+// RETORNE A LARGURA DA JANELA
+const largura = window.innerWidth;
+console.log(largura);
